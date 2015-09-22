33e539a4c5a745c5c9f767fbd467a02f5a4951d7
bk2dcradle <ankitsultana@gmail.com>
2015-09-22 16:28:26 +0530

    Fix

--- includes/js/topcoder.js
+++ includes/js/topcoder.js
@@ -7,7 +7,7 @@
 
 	for ( i = 0; i < jsonData["result"]["ongoing"].length; i++ ) {
 		var temp = jsonData["result"]["ongoing"][i];
-		if (temp["Platform"] != targetPlatform) {
+		if (temp.Platform != targetPlatform) {
 			continue;  
 		}
 		$('.ongoing').append("<div class=\"post-preview\"><a href=\""+temp.url+"\"><h2 class=\"post-title\">"+temp.Name+"</h2></a><p class=\"post-meta\">Hosted by "+temp.Platform+"  | Ends: "+temp.EndTime+"</p><hr>");
