ada7674e3e53840d9baf931bd78669bade14bdef
bk2dcradle <ankitsultana@gmail.com>
2015-11-18 21:30:32 +0530

    Standardise

--- includes/js/active-contests.js
+++ includes/js/active-contests.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-  var req = new XMLHttpRequest();
-  req.open("GET", "http://contesttrackerapi.herokuapp.com/", false);
-  req.send();
-  var jsonData = JSON.parse(req.responseText);
+/* globals $ XMLHttpRequest */
 
-  for ( i = 0; i < jsonData["result"]["ongoing"].length; i++ ) {
-    var temp = jsonData["result"]["ongoing"][i];
-    if (temp.Name == "Are you on Github? Star our Repo if you find it cool!")
-      continue;
-    $('.ongoing').append("<div class=\"post-preview\"><a href=\""+temp.url+"\"><h2 class=\"post-title\">"+temp.Name+"</h2></a><p class=\"post-meta\">Hosted by "+temp.Platform+"  | Ends: "+temp.EndTime+"</p><hr>");
+$(document).ready(function () {
+  var req = new XMLHttpRequest()
+  req.open('GET', 'http://contesttrackerapi.herokuapp.com/', false)
+  req.send()
+  var jsonData = JSON.parse(req.responseText)
+
+  for (var i = 0; i < jsonData['result']['ongoing'].length; i++) {
+    var temp = jsonData['result']['ongoing'][i]
+    if (temp.Name === 'Are you on Github? Star our Repo if you find it cool!') {
+      continue
+    }
+    $('.ongoing').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2></a><p class=\'post-meta\'>Hosted by ' + temp.Platform + '  | Ends: ' + temp.EndTime + '</p><hr>')
   }
-});
+})
