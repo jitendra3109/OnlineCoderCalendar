50ea806631f7ab66c32ff652575bc0c2660c46b3
bk2dcradle <ankitsultana@gmail.com>
2015-11-18 21:34:19 +0530

    Standardise

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,37 +1,37 @@
-$(function() {
-	$('#alertMe').click(function(e) {
-		e.preventDefault();
+/* globals $ */
 
-		$('#successAlert').slideDown();
-	});
+$(function () {
+  $('#alertMe').click(function (e) {
+    e.preventDefault()
 
-	$('a.pop').click(function(e) {
-		e.preventDefault();
-	});
+    $('#successAlert').slideDown()
+  })
 
-	$('a.pop').popover();
+  $('a.pop').click(function (e) {
+    e.preventDefault()
+  })
 
-	$('[rel="tooltip"]').tooltip();
+  $('a.pop').popover()
 
-	/* Custom stuff */
-	$('.btn:not(.btn-link)').hover(function() {
-		var bg_color = $(this).css("background-color");
-		var font_color = $(this).css("color");
-		$(this).css('color', bg_color);
-		$(this).css('background-color', font_color);
-	});
+  $('[rel=\'tooltip\']').tooltip()
 
-	$(window).resize(function() {
-		console.log('resize called');
-		var width=$(window).width();
-		if(width <= 480) {
-			$('#blognail1').removeClass('col-6').addClass('col-12');
-			$('#blognail2').removeClass('col-6').addClass('col-12');
-		}
-		else{
-			$('#blognail1').removeClass('col-12').addClass('col-6');
-			$('#blognail2').removeClass('col-12').addClass('col-6');
-		}
-	}).resize();
+  /* Custom stuff */
+  $('.btn:not(.btn-link)').hover(function () {
+    var bg_color = $(this).css('background-color')
+    var font_color = $(this).css('color')
+    $(this).css('color', bg_color)
+    $(this).css('background-color', font_color)
+  })
 
-});
+  $(window).resize(function () {
+    console.log('resize called')
+    var width = $(window).width()
+    if (width <= 480) {
+      $('#blognail1').removeClass('col-6').addClass('col-12')
+      $('#blognail2').removeClass('col-6').addClass('col-12')
+    } else {
+      $('#blognail1').removeClass('col-12').addClass('col-6')
+      $('#blognail2').removeClass('col-12').addClass('col-6')
+    }
+  }).resize()
+})
