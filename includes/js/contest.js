d7807e68d367a8fb9c6eeff80de2fa70cb5d7fe1
bk2dcradle <ankitsultana@gmail.com>
2015-11-27 18:07:15 +0530

    Use Asynchronous Javascript

--- includes/js/contest.js
+++ includes/js/contest.js
@@ -8,6 +8,9 @@ function changeTimezone (date) {
 }
 
 function parse_url (url) {
+  if (url.indexOf('file') > -1) {
+    return 'codeforces'
+  }
   var matches = url.match(/\.com\/(.*)$/)
   var res = matches[1]
   if (res.indexOf('.html') > -1) {
@@ -18,27 +21,34 @@ function parse_url (url) {
 
 $(document).ready(function () {
   var req = new XMLHttpRequest()
-  req.open('GET', 'http://contesttrackerapi.herokuapp.com/', false)
+  req.onload = function () {
+    try {
+      var url = window.location.href
+      var targetPlatform = parse_url(url).toUpperCase()
+      var i, temp
+      var jsonData = JSON.parse(this.responseText)
+      for (i = 0; i < jsonData['result']['ongoing'].length; i++) {
+        temp = jsonData['result']['ongoing'][i]
+        var endTime = changeTimezone(temp.EndTime).toString().slice(0, 21)
+        if (temp.Platform !== targetPlatform) {
+          continue
+        }
+        $('.ongoing').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2></a><p class=\'post-meta\'>Hosted by ' + temp.Platform + '  | Ends: ' + endTime + '</p><hr>')
+      }
+      for (i = 0; i < jsonData['result']['upcoming'].length; i++) {
+        temp = jsonData['result']['upcoming'][i]
+        var startTime = changeTimezone(temp.StartTime).toString().slice(0, 21)
+        if (temp['Platform'] !== targetPlatform) {
+          continue
+        }
+        $('.upcoming').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2><h3 class=\'post-subtitle\'>Duration: ' + temp['Duration'] + '</h3></a><p class=\'post-meta\'>Hosted by ' + temp.Platform + '  | Starts: ' + startTime + '</p><hr>')
+      }
+    } catch (err) {
+      console.log('Error while trying to fetch the API')
+      console.log(err)
+      $('.ongoing').append('<div class=\'post-preview\' style=\'text-align: center\'><a href=\'' + '#' + '\'><h2 class=\'post-title\'>' + 'Oops! That request didn\'t go long!!' + '</h2><hr />')
+    }
+  }
+  req.open('GET', 'http://contesttrackerapi.herokuapp.com/')
   req.send()
-  var jsonData = JSON.parse(req.responseText)
-  var url = window.location.href
-  var targetPlatform = parse_url(url).toUpperCase()
-  var i, temp
-  for (i = 0; i < jsonData['result']['ongoing'].length; i++) {
-    temp = jsonData['result']['ongoing'][i]
-    var endTime = changeTimezone(temp.EndTime).toString().slice(0, 21)
-    if (temp.Platform !== targetPlatform) {
-      continue
-    }
-    $('.ongoing').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2></a><p class=\'post-meta\'>Hosted by ' + temp.Platform + '  | Ends: ' + endTime + '</p><hr>')
-  }
-  for (i = 0; i < jsonData['result']['upcoming'].length; i++) {
-    temp = jsonData['result']['upcoming'][i]
-    var startTime = changeTimezone(temp.StartTime).toString().slice(0, 21)
-    if (temp['Platform'] !== targetPlatform) {
-      continue
-    }
-    $('.upcoming').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2><h3 class=\'post-subtitle\'>Duration: ' + temp['Duration'] + '</h3></a><p class=\'post-meta\'>Hosted by ' + temp.Platform + '  | Starts: ' + startTime + '</p><hr>')
-  }
 })
-
--- includes/js/upcoming-contests.js
+++ includes/js/upcoming-contests.js
@@ -9,14 +9,19 @@ function changeTimezone (date) {
 
 $(document).ready(function () {
   var req = new XMLHttpRequest()
-  req.open('GET', 'http://contesttrackerapi.herokuapp.com/', false)
-  req.send()
-  var jsonData = JSON.parse(req.responseText)
-
-  for (var i = 0; i < jsonData['result']['upcoming'].length; i++) {
-    var temp = jsonData['result']['upcoming'][i]
-    var startTime = changeTimezone(temp.StartTime).toString().slice(0, 21)
-    var extLink = temp.Platform.toLowerCase()
-    $('.upcoming').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2><h3 class=\'post-subtitle\'>Duration: ' + temp['Duration'] + '</h3></a><p class=\'post-meta\'>Hosted by <a href=\'http://codingcalendar.com/' + extLink + '\'>' + temp.Platform + '</a>  | Starts: ' + startTime + '</p><hr>')
+  req.onload = function () {
+    var jsonData = JSON.parse(this.responseText)
+    for (var i = 0; i < jsonData['result']['upcoming'].length; i++) {
+      var temp = jsonData['result']['upcoming'][i]
+      var startTime = changeTimezone(temp.StartTime).toString().slice(0, 21)
+      var extLink = temp.Platform.toLowerCase()
+      $('.upcoming').append('<div class=\'post-preview\'><a href=\'' + temp.url + '\'><h2 class=\'post-title\'>' + temp.Name + '</h2><h3 class=\'post-subtitle\'>Duration: ' + temp['Duration'] + '</h3></a><p class=\'post-meta\'>Hosted by <a href=\'http://codingcalendar.com/' + extLink + '\'>' + temp.Platform + '</a>  | Starts: ' + startTime + '</p><hr>')
+    }
   }
+  req.onerror = function () {
+    console.log('Error while trying to fetch the API')
+    $('.ongoing').append('<div class=\'post-preview\' style=\'text-align: center\'><a href=\'' + '#' + '\'><h2 class=\'post-title\'>' + 'Oops! That request didn\'t go long!!' + '</h2><hr />')
+  }
+  req.open('GET', 'http://contesttrackerapi.herokuapp.com/')
+  req.send()
 })
